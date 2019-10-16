3df54817c1dba4e8da574b26dab7d942d98368e4
LeoKnox <LeoKnox@users.noreply.github.com>
2019-10-15 22:49:30 -0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.Write(" Press enter ");
+            Console.WriteLine(" To continue");
+            Console.ReadLine();
+        }
+    }
+}
