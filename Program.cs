cfe1511c3399d70b037d3e6060a95e36f1c9f5d8
LeoKnox <LeoKnox@users.noreply.github.com>
2019-10-16 22:54:47 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,11 +6,13 @@ namespace ConsoleApplication
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            whichPart = 2;
+            Console.WriteLine("Welcome to {0} I give it a {1}!", title, whichPart);
             Console.Write(" Press enter ");
             Console.WriteLine(" To continue");
-            Console.ReadLine();
             string test = " test  ";
+            Console.WriteLine(test.Trim().ToUpper());
+            Console.ReadLine();
         }
     }
 }
