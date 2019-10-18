ee0ccb0a8f8c75a1d9c6a06ddc1184a60a016315
LeoKnox <LeoKnox@users.noreply.github.com>
2019-10-17 22:23:13 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,6 +12,10 @@ namespace ConsoleApplication
             Console.WriteLine(" To continue");
             string test = " test  ";
             Console.WriteLine(test.Trim().ToUpper());
+            var one = "World";
+            var two = 103.4f;
+            var three = DateTime.Now;
+            string.Format("Hello {0}. What time is it? {1:T}. Here's a number {2:0.00}.", one, two, three);
             Console.ReadLine();
         }
     }
