dfcb9343ae28655a38325edd33ba0e7a39ad50ac
LeoKnox <LeoKnox@users.noreply.github.com>
2019-10-21 22:14:54 -0700

    Add files via upload

--- School.cs
+++ School.cs
@@ -6,11 +6,11 @@ namespace SchoolLibrary
 {
     class School
     {
-        string SchoolName;
-        string SchoolAddress;
-        string SchoolCity;
-        string SchoolState;
-        string SchoolZip;
-        string PhoneNumber;
+        string Name {get; set;}
+        string Address{get; set;}
+        string City {get; set;}
+        string State {get; set;}
+        string Zip {get; set;}
+        string PhoneNumber {get; set;}
     }
 }
