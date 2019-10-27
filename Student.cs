5dbdefccfc98380c9782ce83ab06ceeb9273ee97
LeoKnox <LeoKnox@users.noreply.github.com>
2019-10-26 19:39:36 -0700

    Add files via upload

---
+++ Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SchoolLibrary
+{
+    public class Student : Person
+    {
+        public enum GradeLevels { Freshman, Sophomore, Junior, Senior }
+        public GradeLevels  GradeLevel { get; set; }
+
+        public override float ComputeGradeAverage()
+        {
+            return 4.0f;
+        }
+    }
+}
---
+++ Teacher.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace SchoolLibrary
+{
+    public class Teacher : Person
+    {
+        public string Subject { get; set; }
+
+        public override float ComputeGradeAverage()
+        {
+            // todo fix implmentation for later
+            return 0.0f;
+        }
+    }
+}
