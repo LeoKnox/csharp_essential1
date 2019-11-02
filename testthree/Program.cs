de75b1dead5f29d62570bf7e23916c3259342fda
LeoKnox <LeoKnox@users.noreply.github.com>
2019-11-01 22:24:52 -0700

    Update Program.cs

--- testthree/Program.cs
+++ testthree/Program.cs
@@ -7,8 +7,12 @@ namespace testthree
     {
         static void Main(string[] args)
         {
+            enum Binary {"off", "on"};
+            StringBuilder sb  = new StringBuilder("hello ");
+            sb.append("you");
+            sb.appendLine("!");
             School test = new School("Swag", "111111111");
-            Console.WriteLine("I lived!");
+            Console.WriteLine("I live again!");
             String test = Console.ReadLine();
         }
     }
