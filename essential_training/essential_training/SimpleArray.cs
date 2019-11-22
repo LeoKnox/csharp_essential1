93eb037a73c783eecdb7876d9f98966ebed5cce9
LeoKnox <LeoKnox@users.noreply.github.com>
2019-11-21 22:49:01 -0700

    Add files via upload

---
+++ essential_training/essential_training/SimpleArray.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace essential_training
+{
+    class SimpleArray
+    {
+        public string[] Classes;
+
+        public SimpleArray()
+        {
+            Classes = new string[4] { "Warrior", "Scout", "Wizard", "Alchemist" };
+        }
+
+        public override string ToString()
+        {
+            return "There are " + Classes.Length + " and they are: " + Classes.ToString();
+        }
+    }
+}
