7033b6cdf3d0eff486ba2d6d09cd1104757cc3ff
LeoKnox <LeoKnox@users.noreply.github.com>
2019-11-22 23:08:17 -0700

    Add files via upload

---
+++ essential_training/EssentialTrainingTests/SimpleArrayTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using essential_training;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class SimpleArrayTest
+    {
+        [TestMethod]
+        public void TestInstantiation()
+        {
+            var testInstance = new SimpleArray();
+            Assert.AreEqual(testInstance.Classes.Length, 4);
+            Assert.AreEqual(testInstance.Classes[1], "Scout");
+        }
+
+        [TestMethod]
+        public void TestToString()
+        {
+            var testInstance = new SimpleArray();
+            Assert.IsTrue(testInstance.ToString().StartsWith("There are"));
+        }
+    }
+}
