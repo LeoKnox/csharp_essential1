e2b3f9ecf1b3c081c8fedf7cb6ac0c34f8998453
LeoKnox <LeoKnox@users.noreply.github.com>
2019-11-25 19:28:28 -0700

    Add files via upload

--- classes/FirstCaster.cs
+++ classes/FirstCaster.cs
@@ -1,1 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
+namespace TestApplication.Models
+{
+    public class FirstCaster
+    {
+        private int casterLevel;
+        public int FireBall()
+        {
+            Random random = new Random();
+            return casterLevel * (random.Next(8) +1) + casterLevel;
+        }
+    }
+}
