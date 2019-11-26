ce9fcc56bcb064ad222074837626900fbdeb57c9
LeoKnox <LeoKnox@users.noreply.github.com>
2019-11-25 21:48:55 -0700

    Add files via upload

---
+++ essential_training/essential_training/wizardClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace essential_training
+{
+    class wizardClass
+    {
+        public List<string> Wizards { get; set; }
+
+        public wizardClass()
+        {
+            Wizards = new List<string>();
+        }
+
+        public bool IsWizard(string wizard)
+        {
+            return Wizards.Contains(wizard);
+        }
+    }
+}
