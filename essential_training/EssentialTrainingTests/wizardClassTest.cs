922fefd7cdf11ea98b1974d0e81b2bc465c91925
LeoKnox <LeoKnox@users.noreply.github.com>
2019-11-28 13:59:48 -0700

    Add files via upload

---
+++ essential_training/EssentialTrainingTests/wizardClassTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using essential_training;
+
+namespace EssentialTrainingTests
+{
+    [TestClass]
+    public class wizardClassTest
+    {
+        [TestMethod]
+        public void IsWizadTest()
+        {
+            var testInstance = new wizardClass();
+            testInstance.Wizards.Add("Pyro");
+            testInstance.Wizards.Add("Cryo");
+            testInstance.Wizards.Add("Electro");
+
+            // expect to pass
+            Assert.IsTrue(testInstance.IsWizard("Pyro"));
+
+            // expect to fail
+            Assert.IsFalse(testInstance.IsWizard("Barbarian"));
+        }
+    }
+}
