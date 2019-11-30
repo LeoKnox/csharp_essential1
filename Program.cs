5d73883a86daa06561f76c0644fce32f5df7621e
LeoKnox <LeoKnox@users.noreply.github.com>
2019-11-29 23:46:20 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,8 +5,10 @@ namespace testthree
 {
     class Program
     {
+        enum Gusts { world, nurse, goodbye };
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello " + Enum.GetName(typeof(Gusts), 1));
             School test = new School("Swag", "111111111");
             Console.WriteLine("I live!");
             Console.WriteLinte("Writing more text");
