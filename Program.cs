a55f0be568524eaa423c56e5aa2c518e7bebc319
LeoKnox <LeoKnox@users.noreply.github.com>
2019-11-30 22:04:19 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,6 +8,8 @@ namespace testthree
         enum Gusts { world, nurse, goodbye };
         static void Main(string[] args)
         {
+            var listOfNumbers = new int[5] { 1, 3, 5, 7, 9 };
+            listOfNumbers.Where(item => item >= 3)
             Console.WriteLine("Hello " + Enum.GetName(typeof(Gusts), 1));
             School test = new School("Swag", "111111111");
             Console.WriteLine("I live!");
