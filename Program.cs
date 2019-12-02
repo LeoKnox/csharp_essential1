d937469ee82b0e38d6ddda744e614ae5ed72b35a
LeoKnox <LeoKnox@users.noreply.github.com>
2019-12-01 21:04:37 -0800

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace testthree
             var listOfNumbers = new int[5] { 1, 3, 5, 7, 9 };
             listOfNumbers.Where(item => item >= 3)
             Console.WriteLine("Hello " + Enum.GetName(typeof(Gusts), 1));
+            Console.WriteLin($"I've been {listOfNumbers[1]}");
             School test = new School("Swag", "111111111");
             Console.WriteLine("I live!");
             Console.WriteLinte("Writing more text");
